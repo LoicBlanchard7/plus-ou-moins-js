92479405d5327ca1ca5b49d8606a02923d4a12cc
LoicBlanchard7 <90914694+LoicBlanchard7@users.noreply.github.com>
2022-11-02 18:19:57 +0100

    Ajout du système de highscore

--- Version-Web/js/script.js
+++ Version-Web/js/script.js
@@ -1,12 +1,31 @@
 var trialsNumber = 0;
 var entree = document.getElementById("enter");
+var name = document.getElementById("name");
+var place = document.getElementById("place");
+
+var messageName = document.getElementById("messageName");
 var boutonValider = document.getElementById("butonValid");
+var butonSaveScore = document.getElementById("butonSaveScore");
+var butonDeleteScore = document.getElementById("butonDeleteScore");
+
 var resultat = document.getElementById("message");
 var randomNumber = Math.floor(Math.random() * 2) + 1;
+butonSaveScore.disabled=true;
+var listScore = document.getElementById("score");
+
+butonDeleteScore.onclick = function () {
+    deleteScore();
+}
 
 boutonValider.onclick = function () {
     afficheMessage();
 }
+
+butonSaveScore.onclick = function() {
+    takeName();
+}
+
+afficheHighscore();
 
 function compare(value) {
     var retour = 0;
@@ -27,7 +46,9 @@ function afficheMessage() {
                 nouveauMessage = "C'est moins";
                 break;
             case 0:
-                nouveauMessage = "Bravo ! Vous avez trouvé en : " + trialsNumber + " essai(s).";
+                nouveauMessage = "Bravo ! Vous avez trouvé en : " + trialsNumber + " essai(s). <br> Vous pouvez maintenant enregistrer votre Score !";
+                butonSaveScore.disabled=false;
+
                 break;
             case 1:
                 nouveauMessage = "C'est plus";
@@ -36,4 +57,57 @@ function afficheMessage() {
     }
     entree.value = "";
     resultat.innerHTML = nouveauMessage;
+}
+
+function takeName() {
+    var nameTemp = name.value;
+    var nouveauMessage = "";
+    nouveauMessage = nameTemp;
+
+    if (nameTemp!= null && nameTemp != ' ') {
+        nouveauMessage = "Votre score est enregistré !";
+        saveScore();
+    } else {
+        nouveauMessage = "Entre un pseudo valide !";
+    }
+
+    messageName.innerHTML = nouveauMessage;
+}
+
+function saveScore() {
+    localStorage.setItem(name.value, trialsNumber);
+    afficheHighscore();
+}
+
+function deleteScore() {
+    localStorage.clear();
+    afficheHighscore();
+}
+
+function afficheHighscore() {
+    listScore.innerHTML = "";
+
+    let sortScore = [];
+
+    for( let i = 0; i < localStorage.length; i++){
+        sortScore.push([localStorage.key(i), localStorage.getItem(localStorage.key(i))]);
+    }
+
+    sortScore.sort(function(a, b) {
+        return a[1] - b[1];
+    });
+
+    for( let i = 0; i < sortScore.length; i++){
+        listScore.innerHTML += i+1 + " - Pseudo : " + sortScore[i][0] + " - Score : " + sortScore[i][1] + "<br>";
+    }
+
+    for( let i = 0; i < sortScore.length; i++){
+        if(sortScore[i][0] == name.value && sortScore[i][1] == trialsNumber) {
+            place.innerHTML = "Vous êtes à la place numéro : " + (i+1);
+        }
+
+    }
+
+    console.log(sortScore);
+
 }
