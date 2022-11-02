c23f3f810ba723607dbfeeb761c534ea5ce1f09c
LoicBlanchard7 <90914694+LoicBlanchard7@users.noreply.github.com>
2022-11-02 20:04:29 +0100

    Ajout de la configuration
    Ajout du niveau de difficulté

--- Version-Web/js/script.js
+++ Version-Web/js/script.js
@@ -1,12 +1,53 @@
+var charNameMax = 12;
+var maxScoreSave = 10;
+var trialsNumberMax = 0;
 var trialsNumber = 0;
 var entree = document.getElementById("enter");
+var essais = document.getElementById("essais");
 var boutonValider = document.getElementById("butonValid");
 var resultat = document.getElementById("message");
 var randomNumber = Math.floor(Math.random() * 2) + 1;
 
+var butonChooseDifficulty = document.getElementById("butonChooseDifficulty");
+var form = document.getElementById("difficultyCadre");
+
+boutonValider.disabled = true;
+
+
 boutonValider.onclick = function () {
     afficheMessage();
 }
+
+form.addEventListener("submit", function(event) {
+    var data = new FormData(form);
+    var output = "";
+    for (const entry of data) {
+      output = entry[1];
+    };
+
+    switch(output) {
+        case "":
+            logDifficulty.innerHTML = "Choissisez un niveau de difficulté valide !";
+        case "Facile":
+            trialsNumberMax = 15;
+            difficultySelect();
+            logDifficulty.innerHTML = "Niveau de difficulté choisit : facile !";
+        break;
+        case "Moyen":
+            trialsNumberMax = 11;
+            difficultySelect();
+            logDifficulty.innerHTML = "Niveau de difficulté choisit : moyen !";
+
+        break;
+        case "Difficile":
+            trialsNumberMax = 8;
+            difficultySelect();
+            logDifficulty.innerHTML = "Niveau de difficulté choisit : difficile !";
+        break;
+    }
+
+    event.preventDefault();
+  }, false);
 
 function compare(value) {
     var retour = 0;
@@ -18,22 +59,39 @@ function compare(value) {
 function afficheMessage() {
     var proposition = parseInt(entree.value);
     var nouveauMessage = "";
-    if (isNaN(proposition)) {
-        nouveauMessage = "Erreur de saisie";
+    var trialsRemaining = trialsNumberMax - trialsNumber;
+
+    if(trialsRemaining>1) {
+        if (isNaN(proposition)) {
+            nouveauMessage = "Erreur de saisie";
+        } else {
+            trialsNumber++;
+            essais.innerHTML = "Il vous reste : " + (trialsNumberMax - trialsNumber) + " essais.";
+            switch (compare(proposition)) {
+                case -1:
+                    nouveauMessage = "C'est moins";
+                    break;
+                case 0:
+                    nouveauMessage = "Bravo ! Vous avez trouvé en : " + trialsNumber + " essai(s).";
+                    break;
+                case 1:
+                    nouveauMessage = "C'est plus";
+                    break;
+            }
+        }
     } else {
         trialsNumber++;
-        switch (compare(proposition)) {
-            case -1:
-                nouveauMessage = "C'est moins";
-                break;
-            case 0:
-                nouveauMessage = "Bravo ! Vous avez trouvé en : " + trialsNumber + " essai(s).";
-                break;
-            case 1:
-                nouveauMessage = "C'est plus";
-                break;
-        }
+        boutonValider.disabled = true;
+        nouveauMessage = "Perdu ! Vous avez utilisé tout vos essais.";
     }
+
+        
     entree.value = "";
     resultat.innerHTML = nouveauMessage;
+}
+
+function difficultySelect() {
+    butonChooseDifficulty.disabled = true;
+    boutonValider.disabled = false;
+    essais.innerHTML = "Il vous reste : " + (trialsNumberMax - trialsNumber) + " essais.";
 }
